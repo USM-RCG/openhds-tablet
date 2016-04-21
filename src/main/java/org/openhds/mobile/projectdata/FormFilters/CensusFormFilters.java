6f83b6e5853170c88331e55440ff01a6b6a83022
Brent Atkinson <brent.atkinson@gmail.com>
2016-04-21 13:21:41 -0400

    Pushed method access into shared method to limit surface of impl dep.

--- src/main/java/org/openhds/mobile/projectdata/FormFilters/CensusFormFilters.java
+++ src/main/java/org/openhds/mobile/projectdata/FormFilters/CensusFormFilters.java
@@ -2,12 +2,9 @@ package org.openhds.mobile.projectdata.FormFilters;
 
 import org.openhds.mobile.model.core.Location;
 import org.openhds.mobile.projectdata.FormPayloadBuilders.LaunchContext;
-import org.openhds.mobile.repository.DataWrapper;
 import org.openhds.mobile.repository.GatewayRegistry;
 import org.openhds.mobile.repository.gateway.LocationGateway;
 import org.openhds.mobile.repository.gateway.SocialGroupGateway;
-
-import java.util.Map;
 
 import static org.openhds.mobile.projectdata.BiokoHierarchy.HOUSEHOLD_STATE;
 
@@ -31,8 +28,8 @@ public class CensusFormFilters {
         }
     }
 
-    private static boolean hasHeadOfHousehold(LaunchContext ctx, Map<String, DataWrapper> hierarchyPath) {
-        String locationUuid = hierarchyPath.get(HOUSEHOLD_STATE).getUuid();
+    private static boolean hasHeadOfHousehold(LaunchContext ctx) {
+        String locationUuid = ctx.getHierarchyPath().get(HOUSEHOLD_STATE).getUuid();
         SocialGroupGateway socialGroupGateway = GatewayRegistry.getSocialGroupGateway();
         return socialGroupGateway.getFirst(ctx.getContentResolver(),
                 socialGroupGateway.findByLocationUuid(locationUuid)) != null;
@@ -41,14 +38,14 @@ public class CensusFormFilters {
     public static class AddHeadOfHousehold implements FormFilter {
         @Override
         public boolean shouldDisplay(LaunchContext ctx) {
-            return !hasHeadOfHousehold(ctx, ctx.getHierarchyPath());
+            return !hasHeadOfHousehold(ctx);
         }
     }
 
     public static class AddMemberOfHousehold implements FormFilter {
         @Override
         public boolean shouldDisplay(LaunchContext ctx) {
-            return hasHeadOfHousehold(ctx, ctx.getHierarchyPath());
+            return hasHeadOfHousehold(ctx);
         }
     }
 }
