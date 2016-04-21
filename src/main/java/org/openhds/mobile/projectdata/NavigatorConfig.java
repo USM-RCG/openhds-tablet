d7dfda59f165c4d7793cd1f4462d4f18f4b6b847
Brent Atkinson <brent.atkinson@gmail.com>
2016-04-21 11:02:32 -0400

    Minor code cleanup: reformatted code.

--- src/main/java/org/openhds/mobile/projectdata/NavigatorConfig.java
+++ src/main/java/org/openhds/mobile/projectdata/NavigatorConfig.java
@@ -189,26 +189,22 @@ class BiokoModule extends AbstractNavigatorModule {
 
         List<FormBehavior> individualForms = new ArrayList<>();
 
-        individualForms.add(new FormBehavior("bed_net",
-                "bioko.bednetsLabel",
+        individualForms.add(new FormBehavior("bed_net", "bioko.bednetsLabel",
                 new BiokoFormFilters.DistributeBednets(),
                 new BiokoFormPayloadBuilders.DistributeBednets(),
                 new BiokoFormPayloadConsumers.DistributeBednets()));
 
-        individualForms.add(new FormBehavior("spraying",
-                "bioko.sprayingLabel",
+        individualForms.add(new FormBehavior("spraying", "bioko.sprayingLabel",
                 new BiokoFormFilters.SprayHousehold(),
                 new BiokoFormPayloadBuilders.SprayHousehold(),
                 new BiokoFormPayloadConsumers.SprayHousehold()));
 
-        individualForms.add(new FormBehavior("super_ojo",
-                "bioko.superOjoLabel",
+        individualForms.add(new FormBehavior("super_ojo", "bioko.superOjoLabel",
                 new BiokoFormFilters.SuperOjo(),
                 new BiokoFormPayloadBuilders.SuperOjo(),
                 new BiokoFormPayloadConsumers.SuperOjo()));
 
-        individualForms.add(new FormBehavior("duplicate_location",
-                "bioko.duplicateLocationLabel",
+        individualForms.add(new FormBehavior("duplicate_location", "bioko.duplicateLocationLabel",
                 new BiokoFormFilters.DuplicateLocation(),
                 new BiokoFormPayloadBuilders.DuplicateLocation(),
                 new BiokoFormPayloadConsumers.DuplicateLocation()));
@@ -243,38 +239,32 @@ class CensusModule extends AbstractNavigatorModule {
         List<FormBehavior> householdFormList = new ArrayList<>();
         List<FormBehavior> individualFormList = new ArrayList<>();
 
-        FormBehavior pregObFormBehavior = new FormBehavior("pregnancy_observation",
-                "shared.pregnancyObservationLabel",
+        FormBehavior pregObFormBehavior = new FormBehavior("pregnancy_observation", "shared.pregnancyObservationLabel",
                 new UpdateFormFilters.RecordPregnancyObservation(),
                 new UpdateFormPayloadBuilders.RecordPregnancyObservation(),
                 new CensusFormPayloadConsumers.ChainedPregnancyObservation());
 
-        FormBehavior visitPregObFormBehavior = new FormBehavior("visit",
-                "shared.visitLabel",
+        FormBehavior visitPregObFormBehavior = new FormBehavior("visit", "shared.visitLabel",
                 new UpdateFormFilters.StartAVisit(),
                 new UpdateFormPayloadBuilders.StartAVisit(),
                 new CensusFormPayloadConsumers.ChainedVisitForPregnancyObservation(pregObFormBehavior));
 
-        householdFormList.add(new FormBehavior("location",
-                "census.locationLabel",
+        householdFormList.add(new FormBehavior("location", "census.locationLabel",
                 new CensusFormFilters.AddLocation(),
                 new CensusFormPayloadBuilders.AddLocation(),
                 new CensusFormPayloadConsumers.AddLocation()));
 
-        individualFormList.add(new FormBehavior("location_evaluation",
-                "census.evaluateLocationLabel",
+        individualFormList.add(new FormBehavior("location_evaluation", "census.evaluateLocationLabel",
                 new CensusFormFilters.EvaluateLocation(),
                 new CensusFormPayloadBuilders.EvaluateLocation(),
                 new CensusFormPayloadConsumers.EvaluateLocation()));
 
-        individualFormList.add(new FormBehavior("individual",
-                "census.headOfHousholdLabel",
+        individualFormList.add(new FormBehavior("individual", "census.headOfHousholdLabel",
                 new CensusFormFilters.AddHeadOfHousehold(),
                 new CensusFormPayloadBuilders.AddHeadOfHousehold(),
                 new CensusFormPayloadConsumers.AddHeadOfHousehold(visitPregObFormBehavior)));
 
-        individualFormList.add(new FormBehavior("individual",
-                "census.householdMemberLabel",
+        individualFormList.add(new FormBehavior("individual", "census.householdMemberLabel",
                 new CensusFormFilters.AddMemberOfHousehold(),
                 new CensusFormPayloadBuilders.AddMemberOfHousehold(),
                 new CensusFormPayloadConsumers.AddMemberOfHousehold(visitPregObFormBehavior)));
@@ -311,8 +301,7 @@ class UpdateModule extends AbstractNavigatorModule {
         List<FormBehavior> bottomFormList = new ArrayList<>();
 
         // Start a Visit FormBehavior
-        individualFormList.add(new FormBehavior("visit",
-                "shared.visitLabel",
+        individualFormList.add(new FormBehavior("visit", "shared.visitLabel",
                 new UpdateFormFilters.StartAVisit(),
                 new UpdateFormPayloadBuilders.StartAVisit(),
                 new UpdateFormPayloadConsumers.StartAVisit()));
@@ -320,44 +309,38 @@ class UpdateModule extends AbstractNavigatorModule {
         // Register an Internal Inmigration, requires a search to do
         ArrayList<FormSearchPluginModule> searches = new ArrayList<>();
         searches.add(SearchUtils.getIndividualPlugin(ProjectFormFields.Individuals.INDIVIDUAL_UUID, R.string.search_individual_label));
-        individualFormList.add(new FormBehavior("in_migration",
-                "update.internalInMigrationLabel",
+        individualFormList.add(new FormBehavior("in_migration", "update.internalInMigrationLabel",
                 new UpdateFormFilters.RegisterInMigration(),
                 new UpdateFormPayloadBuilders.RegisterInternalInMigration(),
                 new UpdateFormPayloadConsumers.RegisterInMigration(),
                 searches));
 
         // Register an External InMigration form (chained after individual form)
-        FormBehavior externalInMigrationFormBehavior = new FormBehavior("in_migration",
-                "update.externalInMigrationLabel",
+        FormBehavior externalInMigrationFormBehavior = new FormBehavior("in_migration", "update.externalInMigrationLabel",
                 new UpdateFormFilters.RegisterInMigration(),
                 new UpdateFormPayloadBuilders.RegisterExternalInMigration(),
                 new UpdateFormPayloadConsumers.RegisterInMigration());
 
         // Register an Individual for External InMigration (chained with in_migration form)
-        individualFormList.add(new FormBehavior("individual",
-                "update.externalInMigrationLabel",
+        individualFormList.add(new FormBehavior("individual", "update.externalInMigrationLabel",
                 new UpdateFormFilters.RegisterInMigration(),
                 new UpdateFormPayloadBuilders.AddIndividualFromInMigration(),
                 new UpdateFormPayloadConsumers.AddIndividualFromInMigration(externalInMigrationFormBehavior)));
 
         // Register an OutMigration FormBehavior
-        bottomFormList.add(new FormBehavior("out_migration",
-                "update.outMigrationLabel",
+        bottomFormList.add(new FormBehavior("out_migration", "update.outMigrationLabel",
                 new UpdateFormFilters.RegisterOutMigration(),
                 new UpdateFormPayloadBuilders.RegisterOutMigration(),
                 new UpdateFormPayloadConsumers.RegisterOutMigration()));
 
         // Register a Death FormBehavior
-        bottomFormList.add(new FormBehavior("death",
-                "update.deathLabel",
+        bottomFormList.add(new FormBehavior("death", "update.deathLabel",
                 new UpdateFormFilters.RegisterDeath(),
                 new UpdateFormPayloadBuilders.RegisterDeath(),
                 new UpdateFormPayloadConsumers.RegisterDeath()));
 
         // Register a Pregnancy Observation FormBehavior
-        bottomFormList.add(new FormBehavior("pregnancy_observation",
-                "shared.pregnancyObservationLabel",
+        bottomFormList.add(new FormBehavior("pregnancy_observation", "shared.pregnancyObservationLabel",
                 new UpdateFormFilters.RecordPregnancyObservation(),
                 new UpdateFormPayloadBuilders.RecordPregnancyObservation(),
                 null));
@@ -365,12 +348,10 @@ class UpdateModule extends AbstractNavigatorModule {
         // Register a Pregnancy OutCome FormBehavior
         ArrayList<FormSearchPluginModule> daddySearch = new ArrayList<>();
         daddySearch.add(SearchUtils.getIndividualPlugin(ProjectFormFields.PregnancyOutcome.FATHER_UUID, R.string.search_father_label));
-        bottomFormList.add(new FormBehavior("pregnancy_outcome",
-                "update.pregnancyOutcomeLabel",
+        bottomFormList.add(new FormBehavior("pregnancy_outcome", "update.pregnancyOutcomeLabel",
                 new UpdateFormFilters.RecordPregnancyOutcome(),
                 new UpdateFormPayloadBuilders.RecordPregnancyOutcome(),
-                null,
-                daddySearch));
+                null, daddySearch));
 
         formsForStates.put(INDIVIDUAL_STATE, individualFormList);
         formsForStates.put(BOTTOM_STATE, bottomFormList);
