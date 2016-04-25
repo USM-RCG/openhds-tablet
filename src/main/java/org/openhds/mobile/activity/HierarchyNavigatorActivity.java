49fdcebbfd1496cdcbeeecbb75807cbea373efad
Brent Atkinson <brent.atkinson@gmail.com>
2016-04-25 15:08:00 -0400

    Migrated state machine to inner class of nav activity in prep for removal.

--- src/main/java/org/openhds/mobile/activity/HierarchyNavigatorActivity.java
+++ src/main/java/org/openhds/mobile/activity/HierarchyNavigatorActivity.java
@@ -36,8 +36,6 @@ import org.openhds.mobile.provider.DatabaseAdapter;
 import org.openhds.mobile.repository.DataWrapper;
 import org.openhds.mobile.repository.search.EntityFieldSearch;
 import org.openhds.mobile.utilities.OdkCollectHelper;
-import org.openhds.mobile.utilities.StateMachine;
-import org.openhds.mobile.utilities.StateMachine.StateListener;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -46,6 +44,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.openhds.mobile.utilities.ConfigUtils.getResourceString;
 import static org.openhds.mobile.utilities.FormUtils.editIntent;
@@ -723,6 +722,76 @@ public class HierarchyNavigatorActivity extends Activity implements HierarchyNav
         @Override
         public void handleSelectedForm(FormBehavior formBehavior) {
             HierarchyNavigatorActivity.this.launchForm(formBehavior, null);
+        }
+    }
+
+    private interface StateListener {
+        void onEnterState();
+        void onExitState();
+    }
+
+    private static class StateMachine {
+
+        private Set<String> stateSet;
+        private String currentState;
+        private Map<String, Set<StateListener>> stateListeners;
+
+        public StateMachine(Set<String> stateSet, String defaultState) {
+            this.stateSet = stateSet;
+            stateListeners = new HashMap<>();
+            transitionTo(defaultState);
+        }
+
+        public String getState() {
+            return currentState;
+        }
+
+        public void transitionTo(String state) {
+            if (!stateSet.contains(state)) {
+                throw new IllegalStateException("State machine has no such state: " + state);
+            }
+
+            if (state.equals(currentState)) {
+                return;
+            }
+
+            fireOnExitListeners();
+            currentState = state;
+            fireOnEnterListeners();
+        }
+
+        public void registerListener(String state, StateListener stateListener) {
+
+            if (!stateSet.contains(state)) {
+                throw new IllegalStateException("State machine has no such state: " + state);
+            }
+
+            if (stateListeners.get(state) == null) {
+                stateListeners.put(state, new HashSet<StateListener>());
+            }
+            stateListeners.get(state).add(stateListener);
+        }
+
+        private void fireOnExitListeners() {
+            Set<StateListener> listenersToFire = stateListeners.get(currentState);
+            if (listenersToFire == null) {
+                return;
+            }
+
+            for (StateListener listener : listenersToFire) {
+                listener.onExitState();
+            }
+        }
+
+        private void fireOnEnterListeners() {
+            Set<StateListener> listenersToFire = stateListeners.get(currentState);
+            if (listenersToFire == null) {
+                return;
+            }
+
+            for (StateListener listener : listenersToFire) {
+                listener.onEnterState();
+            }
         }
     }
 }
