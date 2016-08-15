7161bfc03bd5f68ee0541bc3e01d122bdf673dad
Brent Atkinson <brent.atkinson@gmail.com>
2016-08-15 15:54:40 -0400

    Modified search query interface to more closely align with search prototype.

--- src/main/java/org/openhds/mobile/activity/SearchableActivity.java
+++ src/main/java/org/openhds/mobile/activity/SearchableActivity.java
@@ -23,10 +23,12 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.FuzzyQuery;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.WildcardQuery;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.openhds.mobile.navconfig.HierarchyPath;
@@ -42,6 +44,7 @@ import java.util.List;
 import java.util.Stack;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.regex.Pattern;
 
 import static org.apache.lucene.util.ReaderUtil.getMergedFieldInfos;
 import static org.openhds.mobile.activity.FieldWorkerActivity.ACTIVITY_MODULE_EXTRA;
@@ -52,6 +55,7 @@ public class SearchableActivity extends ListActivity {
 
     private static final String TAG = SearchableActivity.class.getSimpleName();
 
+    private static final Pattern ID_PATTERN = Pattern.compile("(?i)m\\d+(s\\d+(e\\d+(p\\d+)?)?)?");
     private static final ExecutorService execService = Executors.newSingleThreadExecutor();
     private static final Handler handler = new Handler(Looper.getMainLooper());
 
@@ -65,7 +69,8 @@ public class SearchableActivity extends ListActivity {
 
         final Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            performSearch(intent.getStringExtra(SearchManager.QUERY));
+            String query = intent.getStringExtra(SearchManager.QUERY).toLowerCase();
+            performSearch(query);
         }
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -126,11 +131,34 @@ public class SearchableActivity extends ListActivity {
                     IndexReader indexReader = IndexReader.open(indexDir);
                     IndexSearcher searcher = new IndexSearcher(indexReader);
                     try {
-
+                        String[] split = query.split("\\s+");
                         // construct a query over all known fields
                         BooleanQuery boolQuery = new BooleanQuery();
                         for (FieldInfo fieldInfo : getMergedFieldInfos(indexReader)) {
-                            boolQuery.add(new TermQuery(new Term(fieldInfo.name, query)), BooleanClause.Occur.SHOULD);
+                            switch (fieldInfo.name) {
+                                case "uuid":
+                                case "level":
+                                    // do not query these fields directly
+                                    break;
+                                case "extId":
+                                    for (String part : split) {
+                                        if (ID_PATTERN.matcher(part).matches()) {
+                                            boolQuery.add(
+                                                    new WildcardQuery(new Term(fieldInfo.name, query + "*")),
+                                                    BooleanClause.Occur.SHOULD);
+                                        }
+                                    }
+                                    break;
+                                case "name":
+                                    for (String part : split) {
+                                        if (!ID_PATTERN.matcher(part).matches()) {
+                                            boolQuery.add(new FuzzyQuery(new Term(fieldInfo.name, part)), BooleanClause.Occur.SHOULD);
+                                        }
+                                    }
+                                    break;
+                                default:
+                                    boolQuery.add(new TermQuery(new Term(fieldInfo.name, query)), BooleanClause.Occur.SHOULD);
+                            }
                         }
 
                         // perform the search
--- src/main/java/org/openhds/mobile/search/IndexingService.java
+++ src/main/java/org/openhds/mobile/search/IndexingService.java
@@ -14,6 +14,7 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.search.spell.JaroWinklerDistance;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.openhds.mobile.OpenHDS;
@@ -23,10 +24,12 @@ import org.openhds.mobile.provider.OpenHDSProvider;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ConcurrentSkipListSet;
 
+import static java.util.Arrays.asList;
 import static org.apache.lucene.index.IndexWriterConfig.OpenMode.CREATE_OR_APPEND;
 import static org.apache.lucene.util.Version.LUCENE_36;
 
@@ -70,112 +73,203 @@ public class IndexingService extends IntentService {
     }
 
     private void buildHierarchyIndex(SQLiteDatabase db, IndexWriter writer) throws IOException {
-
         String query = String.format("select %s, lower(%s) as level, %s, %s from %s", OpenHDS.HierarchyItems.COLUMN_HIERARCHY_UUID,
                 OpenHDS.HierarchyItems.COLUMN_HIERARCHY_LEVEL, OpenHDS.HierarchyItems.COLUMN_HIERARCHY_EXTID,
                 OpenHDS.HierarchyItems.COLUMN_HIERARCHY_NAME, OpenHDS.HierarchyItems.TABLE_NAME);
         Cursor c = db.rawQuery(query, new String[]{});
-        try {
-            buildIndex("hierarchy items", c, writer);
-        } finally {
-            c.close();
-        }
-
+        buildIndex("hierarchy items", new SimpleHierarchySource(c), writer);
     }
 
     private void buildLocationIndex(SQLiteDatabase db, IndexWriter writer) throws IOException {
-
         String query = String.format("select %s, 'household' as level, %s, %s from %s", OpenHDS.Locations.COLUMN_LOCATION_UUID,
                 OpenHDS.Locations.COLUMN_LOCATION_EXTID, OpenHDS.Locations.COLUMN_LOCATION_NAME,
                 OpenHDS.Locations.TABLE_NAME);
         Cursor c = db.rawQuery(query, new String[]{});
-        try {
-            buildIndex("locations", c, writer);
-        } finally {
-            c.close();
-        }
-
+        buildIndex("locations", new SimpleHierarchySource(c), writer);
     }
 
     private void buildIndividualIndex(SQLiteDatabase db, IndexWriter writer) throws IOException {
-
-        // lowercase here converts from server level names
-        String query = String.format("select %s, 'individual' as level, %s, %s, %s, %s from %s",
+        String query = String.format("select %s, 'individual' as level, %s," +
+                " ifnull(%s,'') || ' ' || ifnull(%s,'') || ' ' || ifnull(%s,'') as name from %s",
                 OpenHDS.Individuals.COLUMN_INDIVIDUAL_UUID, OpenHDS.Individuals.COLUMN_INDIVIDUAL_EXTID,
-                OpenHDS.Individuals.COLUMN_INDIVIDUAL_FIRST_NAME, OpenHDS.Individuals.COLUMN_INDIVIDUAL_LAST_NAME,
-                OpenHDS.Individuals.COLUMN_INDIVIDUAL_OTHER_NAMES, OpenHDS.Individuals.TABLE_NAME);
+                OpenHDS.Individuals.COLUMN_INDIVIDUAL_FIRST_NAME, OpenHDS.Individuals.COLUMN_INDIVIDUAL_OTHER_NAMES,
+                OpenHDS.Individuals.COLUMN_INDIVIDUAL_LAST_NAME, OpenHDS.Individuals.TABLE_NAME);
         Cursor c = db.rawQuery(query, new String[]{});
-        try {
-            buildIndex("individuals", c, writer);
-        } finally {
-            c.close();
-        }
-
+        buildIndex("individuals", new SimpleHierarchySource(c, "name"), writer);
     }
 
-    private void buildIndex(String indexName, Cursor cursor, IndexWriter writer) throws IOException {
+    private void buildIndex(String indexName, DocumentSource source, IndexWriter writer) throws IOException {
 
         Context ctx = getApplicationContext();
         NotificationManager notificationManager = (NotificationManager) ctx.getSystemService(NOTIFICATION_SERVICE);
 
+        Notification.Builder notificationBuilder = new Notification.Builder(ctx)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("Building search indices")
+                .setContentText("Indexing in progress: " + indexName)
+                .setOngoing(true);
+
         try {
-            if (cursor.moveToFirst()) {
-
-                int totalCount = cursor.getCount(), processed = 0, columns = cursor.getColumnCount();
-
-                // Performance: reuse a single document to reduce in-loop memory allocations
-                Document doc = new Document();
-                Field[] fields = new Field[columns];
-                for (int c = 0; c < columns; c++) {
-                    boolean dataCol = c <= 1;  // first two columns are assumed to be uuid and level
-                    String columnName = cursor.getColumnName(c);
-                    fields[c] = new Field(columnName, "",
-                            dataCol ? Field.Store.YES : Field.Store.NO,
-                            dataCol ? Field.Index.NO : Field.Index.ANALYZED);
-                    doc.add(fields[c]);
-                }
-
-                List<Field> nullFields = new ArrayList<>(columns);
-
+            if (source.next()) {
+                int totalCount = source.size(), processed = 0;
                 do {
-                    // Set field values or remove if null
-                    for (int c = 0; c < columns; c++) {
-                        Field field = fields[c];
-                        String value = cursor.getString(c);
-                        if (value != null) {
-                            field.setValue(value);
-                        } else {
-                            doc.removeField(field.name());
-                            nullFields.add(field);
-                        }
-                    }
-
-                    // Add resulting document
-                    writer.addDocument(doc);
-
-                    // Re-add fields that were removed due to missing values
-                    if (!nullFields.isEmpty()) {
-                        for (Field f : nullFields) {
-                            doc.add(f);
-                        }
-                        nullFields.clear();
-                    }
-
-                    // Update progress in status notification
+                    writer.addDocument(source.getDocument());
                     processed++;
                     if (processed % 1000 == 0) {
-                        notificationManager.notify(NOTIFICATION_ID, new Notification.Builder(ctx)
-                                .setSmallIcon(R.drawable.ic_launcher)
-                                .setContentTitle("Building search indices")
-                                .setContentText("Indexing in progress: " + indexName)
-                                .setProgress(totalCount, processed, false)
-                                .setOngoing(true)
-                                .getNotification());
+                        notificationBuilder.setProgress(totalCount, processed, false);
+                        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.getNotification());
                     }
-                } while (cursor.moveToNext());
+                } while (source.next());
             }
         } finally {
             notificationManager.cancel(NOTIFICATION_ID);
+            source.close();
         }
+    }
+}
+
+interface DocumentSource {
+    boolean next();
+    int size();
+    Document getDocument();
+    void close();
+}
+
+abstract class CursorDocumentSource implements DocumentSource {
+
+    protected final Document document;  // shared instace to reduce memory allocations during indexing
+    protected final Cursor cursor;
+    protected Field[] fields;
+    private List<Field> nullFields;
+
+    CursorDocumentSource(Cursor c) {
+        document = new Document();
+        cursor = c;
+    }
+
+    public abstract Field[] getFields();
+
+    public abstract String getFieldValue(int index);
+
+    @Override
+    public Document getDocument() {
+
+        // add field back to document before we start next
+        if (!nullFields.isEmpty()) {
+            for (Field nullField : nullFields) {
+                document.add(nullField);
+            }
+            nullFields.clear();
+        }
+
+        // manipulate document to match next cursor record, remove fields for null values
+        for (int f = 0; f < fields.length; f++) {
+            Field field = fields[f];
+            String value = getFieldValue(f);
+            if (value != null) {
+                field.setValue(value);
+            } else {
+                document.removeField(field.name());
+                nullFields.add(field);
+            }
+        }
+
+        return document;
+    }
+
+    @Override
+    public boolean next() {
+        boolean isFirst = cursor.isBeforeFirst(), hasNext = cursor.moveToNext();
+        if (isFirst && hasNext) {
+            fields = getFields();
+            nullFields = new ArrayList<>(fields.length);
+            for (Field field : fields) {
+                document.add(field);
+            }
+        }
+        return hasNext;
+    }
+
+    @Override
+    public int size() {
+        return cursor.getCount();
+    }
+
+    @Override
+    public void close() {
+        cursor.close();
+    }
+}
+
+class SimpleHierarchySource extends CursorDocumentSource {
+
+    private final Set<String> nameColumns;
+
+    SimpleHierarchySource(Cursor c, String ... nameCols) {
+        super(c);
+        if (nameCols != null && nameCols.length > 0) {
+            nameColumns = new HashSet<>(asList(nameCols));
+        } else {
+            nameColumns = Collections.emptySet();
+        }
+    }
+
+    @Override
+    public Field[] getFields() {
+        int columns = cursor.getColumnCount();
+        Field[] fields = new Field[columns];
+        for (int c = 0; c < columns; c++) {
+            boolean dataCol = c <= 1;  // first two columns are assumed to be uuid and level
+            String columnName = cursor.getColumnName(c);
+            fields[c] = new Field(columnName, "",
+                    dataCol ? Field.Store.YES : Field.Store.NO,
+                    dataCol ? Field.Index.NO : Field.Index.ANALYZED);
+            document.add(fields[c]);
+        }
+        return fields;
+    }
+
+    @Override
+    public String getFieldValue(int index) {
+        if (nameColumns.contains(fields[index].name())) {
+            return getNameValue(index);
+        } else {
+            return cursor.getString(index);
+        }
+    }
+
+    protected String getNameValue(int index) {
+        String rawValue = cursor.getString(index);
+        if (rawValue == null) {
+            return rawValue;
+        } else {
+            Set<String> names = new HashSet<>();
+            for (String name : rawValue.trim().toLowerCase().split("\\s+")) {
+                name = name.replaceAll("\\W+", "");
+                if (names.contains(name) || containsSimilar(names, name)) {
+                    continue;
+                }
+                names.add(name);
+            }
+            StringBuilder buf = new StringBuilder();
+            for (String name : names) {
+                if (buf.length() > 0) {
+                    buf.append(' ');
+                }
+                buf.append(name);
+            }
+            return buf.toString();
+        }
+    }
+
+    private static final float MAX_SIMILARITY = 0.99f;
+
+    private boolean containsSimilar(Set<String> values, String value) {
+        JaroWinklerDistance jwd = new JaroWinklerDistance();
+        for (String v : values) {
+            if (jwd.getDistance(v, value) > MAX_SIMILARITY)
+                return true;
+        }
+        return false;
     }
 }
