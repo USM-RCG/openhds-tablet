af27b194b17469e5fadc5de77257ca462f5e8bca
Brent Atkinson <brent.atkinson@gmail.com>
2016-08-29 15:07:12 -0400

    Remove unused import.

--- src/main/java/org/openhds/mobile/fragment/navigate/detail/DetailFragment.java
+++ src/main/java/org/openhds/mobile/fragment/navigate/detail/DetailFragment.java
@@ -2,7 +2,6 @@ package org.openhds.mobile.fragment.navigate.detail;
 
 import android.app.Fragment;
 
-import org.openhds.mobile.activity.HierarchyNavigatorActivity;
 import org.openhds.mobile.repository.DataWrapper;
 
 public abstract class DetailFragment extends Fragment {
