6fbe353f656053bc8e1b7e0a84c138be2971c8c5
Brent Atkinson <brent.atkinson@gmail.com>
2017-07-27 17:48:37 +0100

    Used static import to reduce the visual clutter

--- src/main/java/org/openhds/mobile/offlinedb/OfflineDbService.java
+++ src/main/java/org/openhds/mobile/offlinedb/OfflineDbService.java
@@ -4,10 +4,10 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
-import org.openhds.mobile.utilities.SyncUtils;
-
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+
+import static org.openhds.mobile.utilities.SyncUtils.makeOfflineDbAvailable;
 
 public class OfflineDbService extends IntentService {
 
@@ -20,7 +20,7 @@ public class OfflineDbService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         try {
-            SyncUtils.makeOfflineDbAvailable(getApplicationContext());
+            makeOfflineDbAvailable(getApplicationContext());
         } catch (IOException | InterruptedException | NoSuchAlgorithmException e) {
             Log.e(TAG, "failed to make offline db available for installation", e);
         }
