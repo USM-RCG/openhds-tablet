db96ed0c58c6cd668bf10dd452b1466dc36cd6fc
Brent Atkinson <brent.atkinson@gmail.com>
2018-01-22 20:07:28 +0100

    Tidied up log messages

--- src/main/java/org/openhds/mobile/sidecar/DiscoveryListener.java
+++ src/main/java/org/openhds/mobile/sidecar/DiscoveryListener.java
@@ -10,31 +10,31 @@ public class DiscoveryListener implements NsdManager.DiscoveryListener {
 
     @Override
     public void onStartDiscoveryFailed(String serviceType, int errorCode) {
-        Log.i(TAG, "start discovery failed: serviceType= " + serviceType + ", error code: " + errorCode);
+        Log.i(TAG, "start discovery failed: serviceType=" + serviceType + ", error code=" + errorCode);
     }
 
     @Override
     public void onStopDiscoveryFailed(String serviceType, int errorCode) {
-        Log.i(TAG, "stop discovery failed: serviceType= " + serviceType + ", error code: " + errorCode);
+        Log.i(TAG, "stop discovery failed: serviceType=" + serviceType + ", error code=" + errorCode);
     }
 
     @Override
     public void onDiscoveryStarted(String serviceType) {
-        Log.i(TAG, "discovery started: serviceType= " + serviceType);
+        Log.i(TAG, "discovery started: serviceType=" + serviceType);
     }
 
     @Override
     public void onDiscoveryStopped(String serviceType) {
-        Log.i(TAG, "discovery stopped: serviceType= " + serviceType);
+        Log.i(TAG, "discovery stopped: serviceType=" + serviceType);
     }
 
     @Override
     public void onServiceFound(NsdServiceInfo serviceInfo) {
-        Log.i(TAG, "service found: serviceInfo= " + serviceInfo);
+        Log.i(TAG, "service found: serviceInfo=" + serviceInfo);
     }
 
     @Override
     public void onServiceLost(NsdServiceInfo serviceInfo) {
-        Log.i(TAG, "service lost: serviceInfo= " + serviceInfo);
+        Log.i(TAG, "service lost: serviceInfo=" + serviceInfo);
     }
 }
