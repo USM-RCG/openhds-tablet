d2ceae8709a49e3f4066701353ce5df87830d418
Brent Atkinson <brent.atkinson@gmail.com>
2018-01-22 19:59:48 +0100

    Minor reformatting

--- src/main/java/org/openhds/mobile/utilities/UrlUtils.java
+++ src/main/java/org/openhds/mobile/utilities/UrlUtils.java
@@ -9,15 +9,11 @@ import static org.openhds.mobile.utilities.ConfigUtils.getPreferenceString;
 public class UrlUtils {
 
     public static String buildServerUrl(Context context, String path) {
-
-        String openHdsBaseUrl = getPreferenceString(context,
-                R.string.openhds_server_url_key, context.getString(R.string.default_openhds_server_url));
-
+        String openHdsBaseUrl = getPreferenceString(context, R.string.openhds_server_url_key, context.getString(R.string.default_openhds_server_url));
         if (openHdsBaseUrl.trim().isEmpty()) {
             showLongToast(context, R.string.no_server_url);
             return null;
         }
-
         return openHdsBaseUrl + path;
     }
 }
