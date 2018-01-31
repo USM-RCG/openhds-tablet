c99e0d28c2e3495e8893bcc1be067d821611d313
Brent Atkinson <brent.atkinson@gmail.com>
2018-01-31 10:12:50 +0100

    Closes #267: Add option to restrict syncing to wi-fi only

--- src/main/java/org/openhds/mobile/syncadpt/SyncAdapter.java
+++ src/main/java/org/openhds/mobile/syncadpt/SyncAdapter.java
@@ -6,6 +6,8 @@ import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
 import android.content.Context;
 import android.content.SyncResult;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.nsd.NsdManager;
 import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
@@ -33,10 +35,22 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+
         final Context ctx = getContext();
+
         boolean autoSyncEnabled = getPreferenceBool(ctx, ctx.getString(R.string.use_auto_sync_key), true);
         boolean manualSyncRequested = extras.getBoolean(ctx.getString(R.string.manual_sync_key), false);
         boolean sidecarEnabled = getPreferenceBool(ctx, ctx.getString(R.string.use_sidecar_key), false);
+        boolean wifiOnlyEnabled = getPreferenceBool(ctx, ctx.getString(R.string.wifi_sync_key), true);
+
+        if (wifiOnlyEnabled) {
+            ConnectivityManager connMgr = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+            if (connMgr == null || !connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected()) {
+                Log.w(TAG, "user settings require wi-fi, not syncing");
+                return;
+            }
+        }
+
         if (autoSyncEnabled || manualSyncRequested) {
             try {
                 if (sidecarEnabled) {
