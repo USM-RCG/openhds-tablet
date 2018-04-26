42515ff4f6beaffd52cf86ee220678f3cbd6317e
Brent Atkinson <brent.atkinson@gmail.com>
2018-04-26 13:29:54 +0100

    Implemented preferences using appcompat classes

--- src/main/java/org/openhds/mobile/activity/PreferenceActivity.java
+++ src/main/java/org/openhds/mobile/activity/PreferenceActivity.java
@@ -1,116 +1,18 @@
 package org.openhds.mobile.activity;
 
 import android.os.Bundle;
-import android.preference.EditTextPreference;
-import android.preference.MultiSelectListPreference;
-import android.preference.Preference;
-import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 
 import org.openhds.mobile.R;
-import org.openhds.mobile.navconfig.NavigatorConfig;
-import org.openhds.mobile.navconfig.NavigatorModule;
-import org.openhds.mobile.utilities.ConfigUtils;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
 
 import static org.openhds.mobile.utilities.ConfigUtils.getAppFullName;
 
-public class PreferenceActivity extends android.preference.PreferenceActivity implements Preference.OnPreferenceChangeListener {
-
-    EditTextPreference serverUrlPref;
-    EditTextPreference syncHistoryPref;
-    MultiSelectListPreference activeModulesPref;
+public class PreferenceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         setTitle(getAppFullName(this));
-
-        addPreferencesFromResource(R.xml.preferences);
-
-        serverUrlPref = (EditTextPreference) findPreference(getText(R.string.openhds_server_url_key));
-        serverUrlPref.setOnPreferenceChangeListener(this);
-        updateSummary(serverUrlPref, serverUrlPref.getText());
-
-        syncHistoryPref = (EditTextPreference) findPreference(getText(R.string.sync_history_retention_key));
-        syncHistoryPref.setOnPreferenceChangeListener(this);
-        updateSummary(syncHistoryPref, syncHistoryPref.getText());
-
-        NavigatorConfig config = NavigatorConfig.getInstance();
-        Collection<NavigatorModule> modules = config.getModules();
-        Map<String, String> moduleLaunchLabels = new LinkedHashMap<>();
-        for (NavigatorModule module : modules) {
-            moduleLaunchLabels.put(module.getName(), module.getLaunchLabel());
-        }
-
-        activeModulesPref = (MultiSelectListPreference) findPreference(getText(R.string.active_modules_key));
-        activeModulesPref.setOnPreferenceChangeListener(this);
-        activeModulesPref.setEntries(moduleLaunchLabels.values().toArray(new String[]{}));
-        activeModulesPref.setEntryValues(moduleLaunchLabels.keySet().toArray(new String[]{}));
-
-        Set<String> activeModules = ConfigUtils.getMultiSelectPreference(
-                this, getString(R.string.active_modules_key), config.getModuleNames());
-        activeModulesPref.setValues(activeModules);
-        updateSummary(activeModulesPref, activeModules);
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-
-        boolean isValid = validate(preference, newValue);
-        if (isValid) {
-            updateSummary(preference, newValue);
-            return true;
-        }
-        return false;
-    }
-
-    private boolean validate(Preference preference, Object newValue) {
-        if (preference == serverUrlPref) {
-            return validateUrl((String)newValue);
-        }
-        return true;
-    }
-
-    private boolean validateUrl(String url) {
-        try {
-            new URL(url);
-        } catch (MalformedURLException e) {
-            new AlertDialog.Builder(PreferenceActivity.this)
-                    .setTitle(R.string.malformed_url)
-                    .setMessage(R.string.malformed_url_msg)
-                    .show();
-            return false;
-        }
-        return true;
-    }
-
-    private void updateSummary(Preference preference, Object newValue) {
-        if (preference instanceof EditTextPreference) {
-            preference.setSummary((String) newValue);
-        } else if (preference instanceof MultiSelectListPreference) {
-            MultiSelectListPreference p = (MultiSelectListPreference) preference;
-            if (newValue instanceof Set) {
-                Set<CharSequence> selectedLabels = new LinkedHashSet<>();
-                CharSequence[] valueLabels = p.getEntries();
-                for (Object value : (Set<?>) newValue) {
-                    int valueIndex = p.findIndexOfValue(value.toString());
-                    if (valueIndex >= 0) {
-                        selectedLabels.add(valueLabels[valueIndex]);
-                    }
-                }
-                preference.setSummary(String.valueOf(selectedLabels));
-            } else {
-                preference.setSummary(String.valueOf(newValue));
-            }
-        }
+        setContentView(R.layout.preferences);
     }
 }
