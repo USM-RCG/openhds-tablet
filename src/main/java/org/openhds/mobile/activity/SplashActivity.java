15222a4c87554e494cfa19a57afba3cd4f4b8fe1
Brent Atkinson <brent.atkinson@gmail.com>
2018-08-01 14:25:22 +0100

    Fixed #275: Permission denied when launching forms on Android N

--- src/main/java/org/openhds/mobile/activity/SplashActivity.java
+++ src/main/java/org/openhds/mobile/activity/SplashActivity.java
@@ -1,10 +1,15 @@
 package org.openhds.mobile.activity;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
@@ -20,13 +25,44 @@ import java.util.List;
  */
 public class SplashActivity extends AppCompatActivity {
 
+    private static final int STORAGE_PERMISSION_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (!isODKInstalled()) {
             promptODKInstall();
+        } else if (needsPermissions()) {
+            askForPermissions();
         } else {
             startApp();
+        }
+    }
+
+    private boolean needsPermissions() {
+        return ContextCompat.checkSelfPermission(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(
+                        this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void askForPermissions() {
+        if (needsPermissions()) {
+            String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(this, permissions,  STORAGE_PERMISSION_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case STORAGE_PERMISSION_REQUEST: {
+                boolean granted = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                if (granted) {
+                    startApp();
+                }
+            }
         }
     }
 
