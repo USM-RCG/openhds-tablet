f8ecdaa50766645782ce213b5d3eb14eb320e30f
Brent Atkinson <brent.atkinson@gmail.com>
2019-05-02 16:41:19 -0400

    BIMEP-660: Disabling unused modules for the 2019 campaign(s)

--- src/main/resources/init.js
+++ src/main/resources/init.js
@@ -1,10 +1,8 @@
 'use strict';
 
-['census.js',
- 'gisops.js',
+['gisops.js',
  'advspray.js',
  'spraying.js',
- 'sbcc.js',
  'netreg.js',
  'mis.js'].forEach(function(m) {
    config.executeScript(m);
