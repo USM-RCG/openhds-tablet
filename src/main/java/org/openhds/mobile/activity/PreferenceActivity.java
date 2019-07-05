7f8e447883473315f41383bf91af2b85b8587431
Brent Atkinson <brent.atkinson@gmail.com>
2019-07-05 10:51:11 -0400

    Added toolbar with up navigation to preferences

--- src/main/java/org/openhds/mobile/activity/PreferenceActivity.java
+++ src/main/java/org/openhds/mobile/activity/PreferenceActivity.java
@@ -1,8 +1,10 @@
 package org.openhds.mobile.activity;
 
 import android.os.Bundle;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.widget.Toolbar;
 import org.openhds.mobile.R;
 
 import static org.openhds.mobile.utilities.ConfigUtils.getAppFullName;
@@ -14,5 +16,12 @@ public class PreferenceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle(getAppFullName(this));
         setContentView(R.layout.preferences);
+        Toolbar toolbar = findViewById(R.id.preferences_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
     }
 }
