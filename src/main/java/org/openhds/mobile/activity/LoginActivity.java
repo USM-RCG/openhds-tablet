3054863d4eb343fa4b8503d948348ed6818eb7f2
Brent Atkinson <brent.atkinson@gmail.com>
2019-07-05 10:12:32 -0400

    Using drawer on login page

--- src/main/java/org/openhds/mobile/activity/LoginActivity.java
+++ src/main/java/org/openhds/mobile/activity/LoginActivity.java
@@ -2,33 +2,42 @@ package org.openhds.mobile.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 import org.openhds.mobile.R;
 
 import static org.openhds.mobile.utilities.ConfigUtils.getAppFullName;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle(getAppFullName(this));
         setContentView(R.layout.login);
+        Toolbar toolbar = findViewById(R.id.login_toolbar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navView = findViewById(R.id.login_navigation_view);
+        navView.setNavigationItemSelectedListener(this);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
+        setTitle(getAppFullName(this));
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.login_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.configure_server:
                 startActivity(new Intent(this, PreferenceActivity.class));
@@ -37,7 +46,7 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(this, SupervisorActivity.class));
                 return true;
             default:
-                return super.onOptionsItemSelected(item);
+                return false;
         }
     }
 }
