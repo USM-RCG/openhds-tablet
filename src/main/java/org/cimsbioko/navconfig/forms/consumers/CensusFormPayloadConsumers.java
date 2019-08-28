33b20a4e38848e261eb24573459b3f0d9896ec1e
Brent Atkinson <brent.atkinson@gmail.com>
2019-08-27 22:01:48 -0400

    Removing the ability to create a location within another sector

--- src/main/java/org/cimsbioko/navconfig/forms/consumers/CensusFormPayloadConsumers.java
+++ src/main/java/org/cimsbioko/navconfig/forms/consumers/CensusFormPayloadConsumers.java
@@ -2,7 +2,6 @@ package org.cimsbioko.navconfig.forms.consumers;
 
 import org.cimsbioko.model.core.Individual;
 import org.cimsbioko.model.core.Location;
-import org.cimsbioko.model.core.LocationHierarchy;
 import org.cimsbioko.navconfig.ProjectFormFields;
 import org.cimsbioko.navconfig.forms.LaunchContext;
 import org.cimsbioko.navconfig.forms.UsedByJSConfig;
@@ -12,53 +11,12 @@ import org.cimsbioko.repository.DataWrapper;
 import org.cimsbioko.repository.GatewayRegistry;
 import org.cimsbioko.repository.gateway.IndividualGateway;
 import org.cimsbioko.repository.gateway.LocationGateway;
-import org.cimsbioko.repository.gateway.LocationHierarchyGateway;
-import org.cimsbioko.utilities.IdHelper;
 
 import java.util.Map;
 
 import static org.cimsbioko.navconfig.BiokoHierarchy.HOUSEHOLD;
-import static org.cimsbioko.navconfig.BiokoHierarchy.SECTOR;
-import static org.cimsbioko.navconfig.forms.builders.PayloadTools.flagForReview;
 
 public class CensusFormPayloadConsumers {
-
-    private static void ensureLocationSectorExists(Map<String, String> payload) {
-
-        LocationHierarchyGateway gw = GatewayRegistry.getLocationHierarchyGateway();
-
-        // lookup relevant values from the form
-        String formMapUuid = payload.get(ProjectFormFields.Locations.HIERARCHY_PARENT_UUID);
-        String formSectorUuid = payload.get(ProjectFormFields.Locations.HIERARCHY_UUID);
-        String formSectorName = payload.get(ProjectFormFields.Locations.SECTOR_NAME);
-
-        // compute the sector's expected extid based on embedded map uuid and specified sector name
-        LocationHierarchy mapArea = gw.getFirst(gw.findById(formMapUuid));
-        String computedSectorExtId = mapArea.getExtId().replaceFirst("^(M\\d+)\\b", "$1\\" + formSectorName);
-
-        // lookup the sectors by expected extid and embedded sector uuid
-        LocationHierarchy sectorByUuid = gw.getFirst(gw.findById(formSectorUuid));
-        LocationHierarchy sectorByComputedExtId = gw.getFirst(gw.findByExtId(computedSectorExtId));
-
-        boolean sectorNeedsUpdate = sectorByUuid == null || !computedSectorExtId.equals(sectorByUuid.getExtId());
-
-        if (sectorNeedsUpdate && sectorByComputedExtId == null) {
-            sectorByComputedExtId = new LocationHierarchy();
-            sectorByComputedExtId.setUuid(IdHelper.generateEntityUuid());
-            sectorByComputedExtId.setParentUuid(mapArea.getUuid());
-            sectorByComputedExtId.setExtId(computedSectorExtId);
-            sectorByComputedExtId.setName(formSectorName);
-            sectorByComputedExtId.setLevel(SECTOR);
-            gw.insertOrUpdate(sectorByComputedExtId);
-        }
-
-        if (sectorNeedsUpdate) {
-            payload.put(ProjectFormFields.Locations.HIERARCHY_UUID, sectorByComputedExtId.getUuid());
-            payload.put(ProjectFormFields.Locations.HIERARCHY_PARENT_UUID, sectorByComputedExtId.getParentUuid());
-            payload.put(ProjectFormFields.Locations.HIERARCHY_EXTID, sectorByComputedExtId.getExtId());
-            flagForReview(payload);
-        }
-    }
 
     private static Location insertOrUpdateLocation(Map<String, String> formPayload) {
         Location location = LocationFormAdapter.fromForm(formPayload);
@@ -78,7 +36,6 @@ public class CensusFormPayloadConsumers {
 
         @Override
         public ConsumerResult consumeFormPayload(Map<String, String> formPayload, LaunchContext ctx) {
-            ensureLocationSectorExists(formPayload);
             insertOrUpdateLocation(formPayload);
             return new ConsumerResult(true);
         }
