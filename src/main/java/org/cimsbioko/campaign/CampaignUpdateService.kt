e71bf3c35a315ffa1d005ab97121290849d385f4
Brent Atkinson <brent.atkinson@gmail.com>
2020-07-11 22:45:43 -0400

    Converted campaign utility class and update service to kotlin

---
+++ src/main/java/org/cimsbioko/campaign/CampaignUpdateService.kt
@@ -0,0 +1,103 @@
+package org.cimsbioko.campaign
+
+import android.accounts.AuthenticatorException
+import android.accounts.OperationCanceledException
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.JobIntentService
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import org.cimsbioko.R
+import org.cimsbioko.utilities.*
+import org.cimsbioko.utilities.AccountUtils.token
+import org.cimsbioko.utilities.CampaignUtils.campaignHash
+import org.cimsbioko.utilities.CampaignUtils.campaignTempFile
+import org.cimsbioko.utilities.CampaignUtils.campaignTempFingerprintFile
+import org.cimsbioko.utilities.CampaignUtils.downloadedCampaignExists
+import org.cimsbioko.utilities.CampaignUtils.getCampaignUrl
+import org.cimsbioko.utilities.CampaignUtils.getCampaigns
+import org.json.JSONException
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
+
+class CampaignUpdateService : JobIntentService() {
+
+    override fun onHandleWork(intent: Intent) {
+        val ctx = applicationContext
+        val wifiOnlyEnabled = ConfigUtils.getPreferenceBool(ctx, ctx.getString(R.string.wifi_sync_key), true)
+        if (wifiOnlyEnabled && !NetUtils.isWiFiConnected()) {
+            Log.i(TAG, "ignoring update request, not on wi-fi")
+            return
+        }
+        if (NetUtils.isConnected() && downloadedCampaignExists()) {
+            try {
+                val token = token
+                val hash = campaignHash
+                val existingCampaignId = SetupUtils.getCampaignId()
+                val campaigns = getCampaigns(token)
+                if (campaigns.length() > 0) {
+                    val firstCampaign = campaigns.getJSONObject(0)
+                    val newCampaignId = firstCampaign.getString("uuid")
+                    val newCampaignName = firstCampaign.getString("name")
+                    val campaignChanged = existingCampaignId != newCampaignId
+                    if (campaignChanged) {
+                        Log.i(TAG, "campaign changed, old = " + existingCampaignId + ", new = " +
+                                newCampaignId + " (" + newCampaignName + ")")
+                    }
+                    val campaignUrl = URL(getCampaignUrl(newCampaignId))
+                    val existingEtag = if (campaignChanged) null else hash
+                    val c = HttpUtils.get(campaignUrl, null, HttpUtils.encodeBearerCreds(token), existingEtag)
+                    when (val response = c.responseCode) {
+                        HttpURLConnection.HTTP_OK -> {
+                            Log.i(TAG, "campaign update available")
+                            try {
+                                val newFile = campaignTempFile
+                                val newFingerprintFile = campaignTempFingerprintFile
+                                IOUtils.streamToFile(c.inputStream, newFile)
+                                val newEtag = c.getHeaderField("ETag")
+                                val campaignId = c.getHeaderField(CIMS_CAMPAIGN_ID)
+                                if (newEtag != null) {
+                                    IOUtils.store(newFingerprintFile, newEtag)
+                                }
+                                val updateMsg = Intent(CAMPAIGN_UPDATE_AVAILABLE)
+                                updateMsg.putExtra("campaignChanged", campaignChanged)
+                                updateMsg.putExtra("campaignId", campaignId)
+                                LocalBroadcastManager
+                                        .getInstance(applicationContext)
+                                        .sendBroadcast(updateMsg)
+                            } catch (e: InterruptedException) {
+                                Log.w(TAG, "campaign download interrupted")
+                            }
+                        }
+                        HttpURLConnection.HTTP_NOT_MODIFIED -> Log.i(TAG, "campaign is up-to-date")
+                        else -> Log.e(TAG, "unexpected http response: $response")
+                    }
+                } else {
+                    Log.i(TAG, "no campaign assigned to device")
+                }
+            } catch (e: AuthenticatorException) {
+                Log.d(TAG, "aborting campaign update request, failed to get token: " + e.message)
+            } catch (e: OperationCanceledException) {
+                Log.d(TAG, "aborting campaign update request, failed to get token: " + e.message)
+            } catch (e: IOException) {
+                Log.d(TAG, "aborting campaign update request, failed to get token: " + e.message)
+            } catch (e: JSONException) {
+                Log.d(TAG, "aborting campaign update request, failed to get token: " + e.message)
+            }
+        }
+    }
+
+    companion object {
+
+        private val TAG = CampaignUpdateService::class.java.simpleName
+        const val CAMPAIGN_UPDATE_AVAILABLE = "CAMPAIGN_UPDATE_AVAILABLE"
+        const val CIMS_CAMPAIGN_ID = "cims-campaign-id"
+        private const val JOB_ID = 0xFA
+
+        @JvmStatic
+        fun enqueueWork(context: Context, intent: Intent) {
+            enqueueWork(context, CampaignUpdateService::class.java, JOB_ID, intent)
+        }
+    }
+}
---
+++ src/main/java/org/cimsbioko/utilities/CampaignUtils.kt
@@ -0,0 +1,168 @@
+package org.cimsbioko.utilities
+
+import android.util.Log
+import org.cimsbioko.App
+import org.cimsbioko.campaign.CampaignUpdateService
+import org.cimsbioko.navconfig.NavigatorConfig
+import org.cimsbioko.scripting.JsConfig
+import org.cimsbioko.utilities.UrlUtils.buildServerUrl
+import org.json.JSONArray
+import org.json.JSONException
+import java.io.*
+import java.net.*
+
+object CampaignUtils {
+
+    private val TAG = CampaignUtils::class.java.simpleName
+
+    @JvmStatic
+    val campaignUrl: String?
+        get() = buildServerUrl(App.getApp(), "/api/rest/campaign")
+
+    @JvmStatic
+    fun getCampaignUrl(uuid: String): String? {
+        return buildServerUrl(App.getApp(), "/api/rest/campaign/$uuid")
+    }
+
+    private val myCampaignsUrl: String?
+        get() = buildServerUrl(App.getApp(), "/api/rest/mycampaigns")
+
+    private val externalCampaignFile: File
+        get() = File(IOUtils.getExternalDir(), SetupUtils.CAMPAIGN_FILENAME)
+
+    @JvmStatic
+    val downloadedCampaignFile: File
+        get() = App.getApp().getFileStreamPath(SetupUtils.CAMPAIGN_FILENAME)
+
+    @JvmStatic
+    val campaignTempFingerprintFile: File
+        get() = FileUtils.getFingerprintFile(campaignTempFile)
+
+    @JvmStatic
+    val campaignTempFile: File
+        get() = FileUtils.getTempFile(downloadedCampaignFile)
+
+    @JvmStatic
+    val campaignHash: String
+        get() = IOUtils.loadFirstLine(downloadedCampaignFingerprintFile)
+
+    private val downloadedCampaignFingerprintFile: File
+        get() = FileUtils.getFingerprintFile(downloadedCampaignFile)
+
+    @JvmStatic
+    fun updateCampaign(campaignId: String?, clearActiveModules: Boolean) {
+        val newFile = campaignTempFile
+        val newFingerprintFile = campaignTempFingerprintFile
+        if (newFile.canRead() && newFingerprintFile.canRead()) {
+            val succeeded = (newFile.renameTo(downloadedCampaignFile)
+                    && newFingerprintFile.renameTo(downloadedCampaignFingerprintFile))
+            if (succeeded) {
+                if (clearActiveModules) {
+                    ConfigUtils.clearActiveModules()
+                }
+                SetupUtils.setCampaignId(campaignId)
+                NavigatorConfig.getInstance().reload()
+                SyncUtils.checkForUpdate()
+            } else {
+                Log.w(TAG, "failed to install campaign update: move failed")
+            }
+        } else {
+            Log.w(TAG, "campaign update failed: new campaign files don't exist or can't be read")
+        }
+    }
+
+    /**
+     * Chooses the [ClassLoader] that the application should use to load its configuration from. It selects the
+     * first existing, readable configuration in order: external storage, downloaded (app files), apk (compiled-in).
+     *
+     * @return the loader to use to load campaign configuration
+     * @throws MalformedURLException
+     */
+    @get:Throws(MalformedURLException::class)
+    private val loader: ClassLoader
+        get() {
+            val possible = arrayOf(externalCampaignFile, downloadedCampaignFile)
+            for (file in possible) if (file.canRead()) {
+                Log.i(TAG, "loading campaign from $file")
+                val base = "jar:file:" + file.path + "!/"
+                val urls = arrayOf(URL(base + "mobile/"), URL(base + "shared/"))
+                return URLClassLoader.newInstance(urls)
+            }
+            Log.i(TAG, "loading internal campaign")
+            return CampaignUtils::class.java.classLoader!!
+        }
+
+    @JvmStatic
+    fun downloadedCampaignExists() = downloadedCampaignFile.canRead()
+
+    @JvmStatic
+    @Throws(MalformedURLException::class, URISyntaxException::class)
+    fun loadCampaign(): JsConfig = JsConfig(loader).load()
+
+    /**
+     * Calls server web api to retrieve the device's currently assigned campaigns.
+     *
+     * @param token the token to use when authenticating
+     * @return a [JSONObject] containing the response data.
+     * @throws IOException   when url is bad, or io fails
+     * @throws JSONException when construction of the response object fails
+     */
+    @JvmStatic
+    @Throws(IOException::class, JSONException::class)
+    fun getCampaigns(token: String?): JSONArray {
+        val urlConn = URL(myCampaignsUrl).openConnection() as HttpURLConnection
+        urlConn.requestMethod = "GET"
+        urlConn.setRequestProperty("Content-Type", "application/json")
+        urlConn.setRequestProperty("Authorization", HttpUtils.encodeBearerCreds(token))
+        var `in`: BufferedInputStream? = null
+        return try {
+            if (urlConn.responseCode != HttpURLConnection.HTTP_OK || !urlConn.contentType.startsWith("application/json")) {
+                throw IOException(String.format(
+                        "unexpected response: status %s, mime type = %s",
+                        urlConn.responseCode, urlConn.contentType))
+            } else {
+                val bout = ByteArrayOutputStream()
+                `in` = BufferedInputStream(urlConn.inputStream)
+                IOUtils.copy(`in`, bout)
+                JSONArray(bout.toString())
+            }
+        } finally {
+            IOUtils.close(`in`)
+            urlConn.disconnect()
+        }
+    }
+
+    /**
+     * Calls server web api to retrieve the campaign file for the specified id.
+     *
+     * @param token the token to use when authenticating
+     * @param uuid  the uuid of the campaign to download
+     * @param targetFile the file to download the campaign to
+     * @return a [File] to the downloaded campaign file.
+     * @throws IOException when url is bad, or io fails
+     */
+    @JvmStatic
+    @Throws(IOException::class)
+    fun downloadCampaignFile(token: String?, uuid: String, targetFile: File?): CampaignDownloadResult {
+        val urlConn = HttpUtils.get(URL(getCampaignUrl(uuid)), null, HttpUtils.encodeBearerCreds(token), null)
+        var `in`: InputStream? = null
+        var out: OutputStream? = null
+        return try {
+            if (urlConn.responseCode != HttpURLConnection.HTTP_OK || !urlConn.contentType.startsWith("application/zip")) {
+                throw RuntimeException(String.format(
+                        "unexpected response: status %s, mime type = %s",
+                        urlConn.responseCode, urlConn.contentType))
+            } else {
+                val etag = urlConn.getHeaderField("ETag")
+                val campaignId = urlConn.getHeaderField(CampaignUpdateService.CIMS_CAMPAIGN_ID)
+                out = FileOutputStream(targetFile)
+                `in` = BufferedInputStream(urlConn.inputStream)
+                IOUtils.copy(`in`, out)
+                CampaignDownloadResult(targetFile, campaignId, etag)
+            }
+        } finally {
+            IOUtils.close(`in`, out)
+            urlConn.disconnect()
+        }
+    }
+}
