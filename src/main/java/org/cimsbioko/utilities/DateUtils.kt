33246336cd31149da3d9936f01f13493700f82ae
Brent Atkinson <brent.atkinson@gmail.com>
2020-07-11 23:27:32 -0400

    Converted date utility class to kotlin

---
+++ src/main/java/org/cimsbioko/utilities/DateUtils.kt
@@ -0,0 +1,17 @@
+package org.cimsbioko.utilities
+
+import android.annotation.SuppressLint
+import org.cimsbioko.navconfig.UsedByJSConfig
+import java.text.SimpleDateFormat
+import java.util.*
+
+@SuppressLint("SimpleDateFormat")
+object DateUtils {
+
+    @UsedByJSConfig
+    fun formatTime(date: Date?): String = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date)
+
+    @UsedByJSConfig
+    fun formatDate(date: Date?): String = SimpleDateFormat("yyyy-MM-dd").format(date)
+
+}
