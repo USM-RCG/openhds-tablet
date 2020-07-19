364c2d15e4b7cc7907e7c32883b3a84d7ad8ed1e
Brent Atkinson <brent.atkinson@gmail.com>
2020-07-19 15:51:53 -0400

    Converted search index class to kotlin

---
+++ src/main/java/org/cimsbioko/search/SearchIndex.kt
@@ -0,0 +1,37 @@
+package org.cimsbioko.search
+
+import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.search.SearcherFactory
+import org.apache.lucene.search.SearcherManager
+import org.apache.lucene.store.Directory
+import org.apache.lucene.store.FSDirectory
+import org.cimsbioko.App
+import java.io.File
+import java.io.IOException
+
+class SearchIndex private constructor() {
+
+    private val searcherManager by lazy {
+        val indexFile = File(App.getApp().applicationContext.filesDir, "search-index")
+        val indexDir: Directory = FSDirectory.open(indexFile)
+        SearcherManager(indexDir, SearcherFactory())
+    }
+
+    @Throws(IOException::class)
+    fun acquire(): IndexSearcher = with(searcherManager) {
+        if (!isSearcherCurrent) {
+            maybeRefresh()
+        }
+        return acquire()
+    }
+
+    @Throws(IOException::class)
+    fun release(searcher: IndexSearcher) {
+        searcherManager.release(searcher)
+    }
+
+    companion object {
+        @JvmStatic
+        val instance by lazy { SearchIndex() }
+    }
+}
