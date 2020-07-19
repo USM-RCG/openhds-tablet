5b14eeaa3cc1c3db92a8d7bc2531a4fade72cb67
Brent Atkinson <brent.atkinson@gmail.com>
2020-07-19 11:27:20 -0400

    Converted sync constants to kotlin

--- src/main/java/org/cimsbioko/syncadpt/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.cimsbioko.syncadpt;
-
-public class Constants {
-    public static final String ACCOUNT_TYPE = "org.cimsbioko";
-    public static final String AUTHTOKEN_TYPE_DEVICE = "Device";
-}
-
---
+++ src/main/java/org/cimsbioko/syncadpt/Constants.kt
@@ -0,0 +1,6 @@
+package org.cimsbioko.syncadpt
+
+object Constants {
+    const val ACCOUNT_TYPE = "org.cimsbioko"
+    const val AUTHTOKEN_TYPE_DEVICE = "Device"
+}
