5c1f8409b11cee2812033763ac873583e00c9f94
Brent Atkinson <brent.atkinson@gmail.com>
2020-07-21 21:23:56 -0400

    Converted jsconfig annotation to kotlin

--- src/main/java/org/cimsbioko/navconfig/UsedByJSConfig.java
+++
@@ -1,7 +0,0 @@
-package org.cimsbioko.navconfig;
-
-/**
- * Used to mark code used so we don't accidentally delete it using IDE inspections.
- */
-public @interface UsedByJSConfig {
-}
---
+++ src/main/java/org/cimsbioko/navconfig/UsedByJSConfig.kt
@@ -0,0 +1,7 @@
+package org.cimsbioko.navconfig
+
+/**
+ * Used to mark code used so we don't accidentally delete it using IDE inspections. It is also referenced in the build
+ * so that proguard doesn't remove code intended to be referenced by scripting.
+ */
+annotation class UsedByJSConfig 
