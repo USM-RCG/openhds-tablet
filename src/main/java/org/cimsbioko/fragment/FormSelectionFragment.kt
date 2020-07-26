92cd8a1cfcb2168e77c33178bdc70f3ec36db74a
Brent Atkinson <brent.atkinson@gmail.com>
2020-07-26 11:29:35 -0400

    Converted form selection fragment to kotlin

---
+++ src/main/java/org/cimsbioko/fragment/FormSelectionFragment.kt
@@ -0,0 +1,78 @@
+package org.cimsbioko.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import android.widget.AdapterView.OnItemClickListener
+import androidx.fragment.app.Fragment
+import org.cimsbioko.R
+import org.cimsbioko.navconfig.forms.Binding
+import org.cimsbioko.navconfig.forms.Launcher
+import org.cimsbioko.utilities.LayoutUtils.configureTextWithPayload
+import org.cimsbioko.utilities.LayoutUtils.makeTextWithPayload
+
+class FormSelectionFragment : Fragment() {
+
+    private var listener: FormSelectionListener? = null
+    private var formListAdapter: FormSelectionListAdapter? = null
+
+    interface FormSelectionListener {
+        fun onFormSelected(binding: Binding)
+    }
+
+    override fun onAttach(ctx: Context) {
+        super.onAttach(ctx)
+        if (ctx is FormSelectionListener) {
+            listener = ctx
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.form_selection_fragment, container, false)
+    }
+
+    fun createFormButtons(values: List<Launcher>) {
+        activity?.let { activity ->
+            formListAdapter = FormSelectionListAdapter(activity, R.layout.generic_list_item_white_text, values)
+            activity.findViewById<ListView>(R.id.form_fragment_listview).apply {
+                adapter = formListAdapter
+                onItemClickListener = FormClickListener()
+                (layoutParams as LinearLayout.LayoutParams).setMargins(0, 0, 0,
+                        if (values.isEmpty()) 0 else resources.getDimensionPixelSize(R.dimen.button_list_divider_height)
+                )
+            }
+        }
+    }
+
+    private inner class FormClickListener : OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+            listener?.let { listener ->
+                formListAdapter?.let { adapter ->
+                    adapter.getItem(position)?.let { item ->
+                        listener.onFormSelected(item.binding)
+                    }
+                }
+            }
+        }
+    }
+
+    private inner class FormSelectionListAdapter(
+            context: Context, resource: Int, objects: List<Launcher>
+    ) : ArrayAdapter<Launcher?>(context, resource, objects) {
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val label = formListAdapter?.getItem(position)?.label
+            return (convertView
+                    ?: makeTextWithPayload(activity!!, label, null, label, null, null, R.drawable.form_selector, null, null, true)).also {
+                configureTextWithPayload(activity!!, it as RelativeLayout, label, null, null, null, true)
+            }
+        }
+    }
+}
