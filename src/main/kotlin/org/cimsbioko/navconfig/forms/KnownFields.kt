79c9f6e0d5d608986d94c965d8b2d93de75101ba
Brent Atkinson <brent.atkinson@gmail.com>
2020-08-15 11:32:31 -0400

    Removed 'known' fields that are unused

--- src/main/kotlin/org/cimsbioko/navconfig/forms/KnownFields.kt
+++ src/main/kotlin/org/cimsbioko/navconfig/forms/KnownFields.kt
@@ -1,9 +1,7 @@
 package org.cimsbioko.navconfig.forms
 
 object KnownFields {
-    const val FIELD_WORKER_UUID = "fieldWorkerUuid"
     const val FIELD_WORKER_EXTID = "fieldWorkerExtId"
     const val COLLECTION_DATE_TIME = "collectionDateTime"
-    const val ENTITY_UUID = "entityUuid"
     const val ENTITY_EXTID = "entityExtId"
 }
