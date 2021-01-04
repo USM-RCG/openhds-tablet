7cbbce47ec9ff66b74cb01f9937afdf3e3662387
Brent Atkinson <brent.atkinson@gmail.com>
2021-01-03 22:15:41 -0500

    Avoid load-on-resume by using view model for favorites

--- src/main/kotlin/org/cimsbioko/fragment/FavoritesFragment.kt
+++ src/main/kotlin/org/cimsbioko/fragment/FavoritesFragment.kt
@@ -13,10 +13,15 @@ import android.widget.ListView
 import android.widget.ProgressBar
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.cimsbioko.R
 import org.cimsbioko.activity.FieldWorkerActivity
 import org.cimsbioko.activity.HierarchyNavigatorActivity
@@ -34,15 +39,75 @@ import org.cimsbioko.utilities.configureText
 import org.cimsbioko.utilities.makeText
 import org.cimsbioko.utilities.toLevelIcon
 
+class FavoritesViewModel : ViewModel() {
+
+    sealed class State {
+        object Loading : State()
+        data class Loaded(val items: List<FavoritesFragment.FormattedHierarchyItem>) : State()
+    }
+
+    private val mutableStateFlow = MutableStateFlow<State>(State.Loading)
+    val stateFlow = mutableStateFlow.asStateFlow()
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        viewModelScope.launch(Dispatchers.IO) {
+            mutableStateFlow.value = State.Loading
+            NavigatorConfig.instance.modules.firstOrNull()?.let { module ->
+                DatabaseAdapter.let { db ->
+                    db.favoriteIds
+                        .map { it to getByHierarchyId(it)?.first }
+                        .partition { (_, item) -> item != null }
+                        .let { (found, lost) ->
+                            lost.forEach { (id, _) -> db.removeFavorite(id) }
+                            found.mapNotNull { (_, item) ->
+                                item?.level
+                                    ?.let { module.getHierFormatter(it) }
+                                    ?.formatItem(item)
+                                    ?.let { FavoritesFragment.FormattedHierarchyItem(item, it) }
+                            }
+                        }
+                }
+            }?.also { mutableStateFlow.value = State.Loaded(it) }
+        }
+    }
+
+    fun removeFavorite(hierId: String) {
+        DatabaseAdapter.removeFavorite(hierId)
+        loadData()
+    }
+}
+
 class FavoritesFragment : Fragment() {
+
+    private val model: FavoritesViewModel by viewModels()
 
     private var list: ListView? = null
     private var progress: ProgressBar? = null
     private var dataAdapter: FavoriteAdapter? = null
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        dataAdapter = FavoriteAdapter(requireActivity())
+        lifecycleScope.launchWhenStarted {
+            model.stateFlow.collect { state ->
+                isLoading = when (state) {
+                    FavoritesViewModel.State.Loading -> true
+                    is FavoritesViewModel.State.Loaded -> {
+                        dataAdapter?.clear()
+                        dataAdapter?.addAll(state.items)
+                        false
+                    }
+                }
+            }
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return FavoritesFragmentBinding.inflate(inflater, container, false).apply {
-            dataAdapter = FavoriteAdapter(requireActivity())
             list = favoritesList
             progress = progressBar
         }.root
@@ -65,11 +130,6 @@ class FavoritesFragment : Fragment() {
         dataAdapter = null
     }
 
-    override fun onResume() {
-        loadData()
-        super.onResume()
-    }
-
     override fun onCreateContextMenu(menu: ContextMenu, v: View, menuInfo: ContextMenuInfo?) {
         if (v.id == R.id.favorites_list) {
             requireActivity().menuInflater.inflate(R.menu.favorite_menu, menu)
@@ -85,9 +145,8 @@ class FavoritesFragment : Fragment() {
             }
             R.id.forget_favorite -> {
                 getItem(info.position)?.let {
-                    DatabaseAdapter.removeFavorite(it.item.hierarchyId)
+                    model.removeFavorite(it.item.hierarchyId)
                     showShortToast(requireContext(), R.string.removed_favorite)
-                    loadData()
                 }
                 return true
             }
@@ -132,8 +191,8 @@ class FavoritesFragment : Fragment() {
     private inner class FavoriteAdapter(
         context: Context
     ) : ArrayAdapter<FormattedHierarchyItem>(context, R.layout.generic_list_item) {
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-            return requireActivity().let { activity ->
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View =
+            requireActivity().let { activity ->
                 (convertView?.let { GenericListItemBinding.bind(it) }
                     ?: makeText(activity, background = R.drawable.data_selector)).also { binding ->
                     getItem(position)?.apply {
@@ -149,34 +208,5 @@ class FavoritesFragment : Fragment() {
                     }
                 }.root
             }
-        }
     }
-
-    private fun loadData() = lifecycleScope.launch {
-
-        isLoading = true
-        dataAdapter?.clear()
-
-        NavigatorConfig.instance.modules.firstOrNull()?.let { module ->
-            withContext(Dispatchers.IO) {
-                DatabaseAdapter.let { db ->
-                    db.favoriteIds
-                        .map { it to getByHierarchyId(it)?.first }
-                        .partition { (_, item) -> item != null }
-                        .let { (found, lost) ->
-                            lost.forEach { (id, _) -> db.removeFavorite(id) }
-                            found.mapNotNull { (_, item) ->
-                                item?.level
-                                    ?.let { module.getHierFormatter(it) }
-                                    ?.formatItem(item)
-                                    ?.let { FormattedHierarchyItem(item, it) }
-                            }
-                        }
-                }
-            }
-        }?.also { dataAdapter?.addAll(it) }
-
-        isLoading = false
-    }
-
 }
