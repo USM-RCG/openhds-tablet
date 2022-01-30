db645f358e393d2d3c4ae8e8241e031bb29ecfc2
Brent Atkinson <brent.atkinson@gmail.com>
2022-01-30 14:53:05 -0500

    Improve default query builder implementation

--- src/main/kotlin/org/cimsbioko/scripting/StubQueryBuilder.kt
+++ src/main/kotlin/org/cimsbioko/scripting/StubQueryBuilder.kt
@@ -1,15 +1,25 @@
 package org.cimsbioko.scripting
 
-import org.cimsbioko.search.BooleanClause
-import org.cimsbioko.search.BooleanQuery
-import org.cimsbioko.search.Query
-import org.cimsbioko.search.SearchQueryBuilder
+import org.cimsbioko.search.*
 
-class StubBooleanQuery : BooleanQuery {
-    override val clauses: List<BooleanClause>
-        get() = emptyList()
-}
+private class StubBooleanQuery(
+        override val clauses: List<BooleanClause> = emptyList()
+) : BooleanQuery
+
+private class StubBooleanClause(
+        override val query: Query,
+        override val occurs: BooleanClause.Occurs = BooleanClause.Occurs.SHOULD
+) : BooleanClause
+
+private class StubFuzzyQuery(
+        override val field: String,
+        override val text: String,
+        override val maxEdits: Int = 2
+) : FuzzyQuery
 
 class StubQueryBuilder : SearchQueryBuilder {
-    override fun build(query: String): Query = StubBooleanQuery()
+    override fun build(query: String): Query = query
+            .split(Regex("\\s+"))
+            .map { StubBooleanClause(StubFuzzyQuery(field = "name", text = it)) }
+            .let { StubBooleanQuery(it) }
 }
